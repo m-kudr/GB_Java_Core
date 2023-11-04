71371355467bea128725c9a5ce0447873724257a
mkudr <mkudr29@google.com>
2023-11-04 11:43:43 +0300

    Add HomeWork for Seminar-3

---
+++ Sem3/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import Package1.Employee;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        Employee employeeIvan = new Employee("Ivan", "Igorevich",
+                2 "Ovchinnikov", "8(495)000-11-22",
+                3 "developer", 50000, 1985);
+    }
+}
---
+++ Sem3/src/main/java/Package1/Employee.java
@@ -0,0 +1,10 @@
+package Package1;
+
+public class Employee {
+    String name;
+    String surname;
+    String phone;
+    String position;
+    int salary;
+    
+}
